8f28611f78d10582b9d440f768f1c7436d1900fb
sbkimm <sebin723@gmail.com>
2024-04-14 18:46:18 +0900

    DM 내부 ChatBox 추가

--- pages/DirectMessage/index.tsx
+++ pages/DirectMessage/index.tsx
@@ -1,8 +1,34 @@
 import Workspace from "@layouts/Workspace";
 import React from "react";
+import { Container, Header } from "./styles";
+import { useParams } from "react-router";
+import useSWR from "swr";
+import fetcher from "@utils/fetcher";
+import gravatar from "gravatar";
+import ChatBox from "@components/ChatBox";
 
 const DirectMessage = () => {
-  return <div>dm</div>;
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+  const { data: userData } = useSWR(
+    `/api/workspaces/${workspace}/users/${id}`,
+    fetcher
+  );
+  const { data: myData } = useSWR("/api/users", fetcher);
+
+  if (!userData || !myData) return null;
+
+  return (
+    <Container>
+      <Header>
+        <img
+          src={gravatar.url(userData.email, { s: "24px", d: "retro" })}
+          alt={userData.nickname}
+        />
+        <span>{userData.nickname}</span>
+      </Header>
+      <ChatBox chat="" />
+    </Container>
+  );
 };
 
 export default DirectMessage;
